1830322799f678288bd1e19b621109f100ac5819
Phillip Chan <phil.chan@sce.com>
2020-07-08 13:41:15 -0700

    Finish splash screen with contraints

---
+++ The Blessing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Blessing
+//
+//  Created by Phillip Chan on 7/8/20.
+//  Copyright © 2020 Phillip Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
